b74a3556a0650e3533e259da5eee3f1298524e26
wildan romiza <38659848+wildanelek12@users.noreply.github.com>
2024-11-18 16:22:30 +0700

    Create navbar.js

---
+++ nextjs/navbar.js
@@ -0,0 +1,39 @@
+<header className="bg-gray-800 shadow-lg">
+<div className="container mx-auto px-4 py-4 flex items-center justify-between">
+  <h1 className="text-2xl font-bold">TMDB Movies</h1>
+  <nav>
+    <ul className="flex space-x-4">
+      <li>
+        <a
+          href="/"
+          className={`hover:text-gray-400 ${
+            isActive("/") ? "text-gray-400" : ""
+          }`}
+        >
+          Home
+        </a>
+      </li>
+      <li>
+        <a
+          href="/movies/popular"
+          className={`hover:text-gray-400 ${
+            isActive("/movies/popular") ? "text-gray-400" : ""
+          }`}
+        >
+          Popular
+        </a>
+      </li>
+      <li>
+        <a
+          href="/movies/top_rated"
+          className={`hover:text-gray-400 ${
+            isActive("/movies/top_rated") ? "text-gray-400" : ""
+          }`}
+        >
+          Top Rated
+        </a>
+      </li>
+    </ul>
+  </nav>
+</div>
+</header>
