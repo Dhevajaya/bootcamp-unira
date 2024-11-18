c3dc86a37041aa1e84039db7168c52b519b83798
Dhevajaya <dhevajaya123@gmail.com>
2024-11-18 15:33:04 +0700

    Initial commit

---
+++ tipedata.js
@@ -0,0 +1,24 @@
+// Contoh Tipe Data String
+let nama = "Ahmad Fathoni";
+console.log("Contoh 1: " + nama);
+
+// Contoh Tipe Data Number
+let umur = 25;
+console.log("Contoh 2: " + umur);
+
+// Contoh Tipe Data Boolean
+let isMarried = false;
+console.log("Contoh 3: " + isMarried);
+
+// Contoh Tipe Data Array
+let hobbies = ["nonton film", "main game", "baca buku"];
+console.log("Contoh 4: " + hobbies);
+
+// Contoh Tipe Data Object
+let person = {
+    nama: "Ahmad Fathoni",
+    umur: 25,
+    isMarried: false,
+    hobbies: ["nonton film", "main game", "baca buku"]
+};
+console.log("Contoh 5: " + person.nama);
---
+++ variabel.js
@@ -0,0 +1,15 @@
+
+
+// Contoh 1: Deklarasi Variabel dengan var
+var nama = "Ahmad Fathoni";
+console.log("Contoh 1: " + nama);
+
+// Contoh 2: Deklarasi Variabel dengan let
+let umur = 25;
+console.log("Contoh 2: " + umur);
+
+// Contoh 3: Deklarasi Variabel dengan const
+const PI = 3.14;
+console.log("Contoh 3: " + PI);
+
+
